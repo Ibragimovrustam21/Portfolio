76cbbfa9a5e2f53029f881f5e8217a59905f4db6
Rustam21 <samsung122356@mail.ru>
2021-08-29 14:22:42 +0500

    'text'

--- Conduit Project in React/src/pages/ProfileUser.js
+++ Conduit Project in React/src/pages/ProfileUser.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { useFetch } from '../component/useFetch'
 import { UserArticles } from './UserArticles'
+
 export const ProfileUser = ({ match, location }) => {
     const slug = match.params.slug
     const apiUrl = `/profiles/${slug}`
@@ -10,8 +11,9 @@ export const ProfileUser = ({ match, location }) => {
     useEffect(() => {
         doFetch()
     }, [doFetch])
+
     if (!response) {
-        return <p className='text-center text-success' > Loading... </p>
+        return <p className='text-center text-success'> Loading... </p>
     }
     return (
         <div className="profile-page" >
@@ -22,11 +24,13 @@ export const ProfileUser = ({ match, location }) => {
                             <img className="user-img"
                                 alt=""
                                 src={response.profile.image}
-                            /> <h4 > {response.profile.username} </h4>
-                            <p> {response.profile.bio} </p> <Link to='/settings'
-                                className='nav-link p-0 btn btn-outline-secondary btnSettings py-1 px-2 action-btn' >
-                                <i className='fas fa-cog' > </i>&nbsp;
-                                Edit porfile settings </Link>
+                            />
+                            <h4 > {response.profile.username} </h4>
+                            <p> {response.profile.bio} </p>
+                            <Link to='/settings' className='nav-link p-0 btn btn-outline-secondary btnSettings py-1 px-2 action-btn' >
+                                <i className='fas fa-cog' ></i>&nbsp;
+                                Edit porfile settings
+                            </Link>
 
                         </div>
                     </div >
@@ -40,12 +44,14 @@ export const ProfileUser = ({ match, location }) => {
                                 <li className="nav-item" >
                                     <NavLink exact to={`/profile/${response.profile.username}`}
                                         className="nav-link" >
-                                        My Posts </NavLink>
+                                        My Posts
+                                    </NavLink>
                                 </li >
                                 <li className="nav-item" >
                                     <NavLink to={`/profile/${response.profile.username}/favorites`}
                                         className="nav-link" >
-                                        Favorites Posts </NavLink>
+                                        Favorites Posts
+                                    </NavLink>
                                 </li >
                             </ul>
                         </div >
